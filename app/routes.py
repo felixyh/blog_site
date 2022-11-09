1697b7e9847b722302b439d2c4b2a42fb33f1ee9
“felixyh” <felixyanghui@gmail.com>
2022-11-09 13:49:09 +0800

    chapter-3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +19,10 @@ def index():
     ]
 
     return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+    
