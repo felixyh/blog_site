c7811f76273693f718c318aa87a140c23a63bacd
“felixyh” <felixyanghui@gmail.com>
2022-11-10 09:52:35 +0800

    update

--- blogsite.py
+++ blogsite.py
@@ -1,10 +1,15 @@
-from app import app
+from app import app, db
 
-# to ensure "flask db migrate -m "users table"" can detect the defined schema
+# # to ensure "flask db migrate -m "users table"" can detect the defined schema
 from app import models 
 
-# from flask_script import Shell
+# from app.models import User, Post
+
+# # from flask_script import Shell
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':
-    
     app.run(host='0.0.0.0', debug=True)
