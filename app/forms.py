42b1fe5edaa1beacbd06637d21a6d00039d3c178
“felixyh” <felixyanghui@gmail.com>
2022-11-17 15:41:40 +0800

    chapter-9

--- app/forms.py
+++ app/forms.py
@@ -54,3 +54,12 @@ class EditProfileForm(FlaskForm):
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
 
+
+# blog submission form
+class PostForm(FlaskForm):
+    post = TextAreaField('Say Something', validators=[
+        DataRequired(), Length(min=1, max=140)
+    ])
+    submit = SubmitField('Submit')
+
+
